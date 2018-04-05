048e68d44e4be5ee9d10df35dd942e27eae904ac
Ben <ben@prestigegifting.co.uk>
2018-04-05 10:57:15 +0100

    Added some changes in reference to 6ba5856d13eff61b8811b2b1bb8ee167345f9695

--- source/plugins/kicks.js
+++ source/plugins/kicks.js
@@ -1,10 +1,24 @@
 module.exports = function (bot) {
     'use strict';
 
+    // Only active in the JS room
+    if (bot.adapter.site !== 'stackoverflow' || bot.adapter.roomid !== 17) {
+        bot.log('Not activating unformatted code checking; not in right room/site');
+        return;
+    }
+
+    // Disgusting way of storing the last message ID for the transcript
+    var lastMsgId = null;
+    bot.IO.register('input', function(msgObj) {
+        lastMsgId = msgObj.id;
+    });
+
     var kicks = bot.memory.get('kicks');
 
     bot.IO.register('kick', function (msgObj) {
 
+        // Message object doesn't contain username of the kickee
+        // We need to retrieve it using this method
         bot.users.findUsername(msgObj.target_user_id, function(username) {
 
             var kickObj = {
@@ -16,38 +30,51 @@ module.exports = function (bot) {
                     id: msgObj.user_id,
                     name: msgObj.user_name,
                     target: msgObj.target_user_id,
-                    target_name: username
+                    targetName: username
                 }
             };
 
             kicks[msgObj.id] = kickObj;
             bot.memory.save('kicks');
-    
-            if (bot.config.githubKey &&  bot.config.githubKey.length) {
+
+            if (bot.config.githubKey && bot.config.githubKey.length) {
+
+                let githubBody = kickObj.content;
+                if (lastMsgId) {
+                    githubBody += '\nhttps://chat.stackoverflow.com/transcript/message/' + lastMsgI;
+                }
+
                 bot.IO.xhr({
                     method: 'POST',
                     url: 'https://api.github.com/repos/' + bot.config.issueRepo + '/issues',
                     headers: {
                         Authorization: 'token ' + bot.config.githubKey,
-                        Accept: 'application/vnd.github.v3+json'
+                        Accept: 'application/vnd.github.v3+json',
+                        'Content-Type': 'application/json'
                     },
                     data: JSON.stringify({
                         title: username + ' kicked by ' + kickObj.who.name,
-                        body: kickObj.content,
+                        body: githubBody,
                         labels: ['kick']
                     }),
                     complete: function(response, xhr) {
                         if (xhr.status === 200) {
                             var responseObj = JSON.parse(response);
-                            bot.adapter.out.add(username + ' has been kicked by ' + kickObj.who.name + '. An issue has been created [here](' + responseObj.url + ').');
+                            bot.adapter.out.add([
+                                username,
+                                'has been kicked by',
+                                kickObj.who.name + '.',
+                                'An issue has been created',
+                                bot.adapter.link('here', responseObj.url) + '.'
+                            ].join(' '), msgObj.room_id);
                         }
                         else {
-                            bot.adapter.out.add('Something went wrong POSTing to the Github API');
+                            bot.adapter.out.add('Something went wrong POSTing to the Github API.', msgObj.room_id);
+                            bot.log(xhr, 'github error');
                         }
                     }
                 });
             }
-
         });
     });
 };
